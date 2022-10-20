e1b0e0a38ee906da737556657a39886d4275d901
Timothée Gallais <timothee.gallais@edu.devinci.fr>
2022-10-20 12:17:02 +0200

    Bonjour

--- src/main/java/com/example/javafxstore/Main.java
+++ src/main/java/com/example/javafxstore/Main.java
@@ -10,15 +10,12 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Store.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
-
-    //Bonjour
-    //Salut
     public static void main(String[] args) {
         launch();
     }
