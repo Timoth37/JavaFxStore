7caffdeccbc5c305862e38b83ac6fdb8bb52f0ee
ellioth <elliot.houdjal@gmail.com>
2022-10-20 19:18:00 +0200

    Interface graphique finie (il manque juste le code, les boutons ne font rien pour l'instant)

--- src/main/java/com/example/javafxstore/Main.java
+++ src/main/java/com/example/javafxstore/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Store.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Women's Store");
         stage.setScene(scene);
         stage.show();
